97fc2eaf2e9a33375d1e28a4b2ad01f1854c3830
sjdubois <scottjdubois@gmail.com>
2015-08-20 15:00:59 -0400

    updates to index.html and app.js

--- js/app.js
+++ js/app.js
@@ -14,4 +14,69 @@ $(document).ready(function(){
 
 });
 
+$(function(){
+
+	var totalGuesses = [];
+
+
+	function guessCount() {
+		$("#count").html('');
+		var NumberOfGuesses = totalGuesses.length;
+		$("#count").append(NumberOfGuesses);
+
+	}
+	
+
+	$("form").submit(function(e){
+		e.preventDefault();
+		guess = $("#userGuess").val();
+
+		totalGuesses.push(guess);
+		
+		$("#guessList").html('');
+
+		var i;
+		for (i = 0; i < totalGuesses.length; i++) {
+			$("#guessList").append('<li>' + totalGuesses[i] + '</li>');
+		}
+		
+		guessCount();
+		
+	});
+
+	
+		
+
+
+	/*function guessCounter() {
+		totalGuesses.push();
+	}*/
+
+
+
+
+
+
+
+// Random Math Function - Generates a Random Integer between 1-100
+
+	/* function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+
+	$('.new').click(function() {
+		var randomNumber = getRandomInt(1, 100);
+		alert(randomNumber);
+	}); */
+
+
+
+
+
+
+
+
+});
+
 
